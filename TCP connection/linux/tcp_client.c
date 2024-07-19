a7682364c073611bacb1b2dcb51d2597114d3fea
Jay Nguyen <bobguy0606@gmail.com>
2024-07-19 14:49:00 -0700

    added comment

--- TCP connection/linux/tcp_client.c
+++ TCP connection/linux/tcp_client.c
@@ -1,73 +1,97 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <sys/socket.h>
-#include <sys/types.h>
-#include <netinet/in.h>
-#include <arpa/inet.h>
-#include <unistd.h>
-#include <string.h>
+#include <sys/socket.h> //socket lib
+#include <sys/types.h>  //data types used in sockets
+#include <netinet/in.h> //structs for domain addresses
+#include <arpa/inet.h>  // for modding IP addresses (numeric)
+#include <unistd.h>     //to close socket
+#include <string.h>     //for memset and strlen
 
-#define SERVER_ADDRESS "98.137.11.164" //yahoo.com
+#define SERVER_ADDRESS "98.137.11.164" // yahoo.com
 #define PORT 80
 
-int main(){
+int main()
+{
 
-//create socket
-int network_socket;
+    ////////////////////////////////////////////////////////
+    // Define necessary information before connection
+    ////////////////////////////////////////////////////////
 
-network_socket = socket(AF_INET, SOCK_STREAM, 0);
+    // declare socket
+    int network_socket;
 
-if(network_socket < 0){
+    char server_response[1024]; // this buffer holds the server response
 
-printf("failed to initialize socket\n");
+    // this data is the request message string, it lets the server know this is a GET request which asks for a web page
+    char *data;
 
+    data = "GET / HTTP/1.0\r\n\r\n";
+    // explanation of data message:
+    /*
+    GET: requesting to retrieve(GET) data
+    / : resource wanted is at root of server
+    HTTP/1.0: version of protocol wanted
+    \r\n\r\n: first pair - end of request line, second pair - end of headers
 
-return -1;
+    */
 
+    /*
+     AF_INET = address family of ipv4
+     SOCK_STREAM = define what type of socket to use, this socket should provide a continuous stream of data
+     0 = identifier number for the protocol desired, 0 means default protocol, which is TCP
+
+    */
+    network_socket = socket(AF_INET, SOCK_STREAM, 0);
+
+    if (network_socket < 0)
+    {
+
+        printf("failed to initialize socket\n");
+
+        return -1;
+    }
+
+    // server address struct
+    struct sockaddr_in server_address;
+
+    server_address.sin_addr.s_addr = inet_addr(SERVER_ADDRESS); // converts the server address from stringn to network address
+    server_address.sin_port = htons(PORT);                      // converts port number from host byte order to network byte order
+    server_address.sin_family = AF_INET;                        // address family is of IPV4
+
+    ////////////////////////////////////////////////////////
+    // Start connection
+    ////////////////////////////////////////////////////////
+
+    // try to connect
+    int connection_status = connect(network_socket, (struct sockaddr *)&server_address, sizeof(server_address));
+
+    if (connection_status == -1)
+    {
+
+        printf("Connection not established, an error occured\n");
+        return -1;
+    }
+
+    printf("Connection established\n");
+
+    ////////////////////////////////////////////////////////
+    // Send and listen for messages
+    ////////////////////////////////////////////////////////
+
+    // connection has been established, thus send the request message through the socket
+    write(network_socket, data, strlen(data));
+
+    // clear buffer, just to avoid zombie bytes
+    memset(server_response, 0, 1024);
+
+    // listens to server responses, then put server response into buffer
+    read(network_socket, server_response, 1023); // exclude null terminator
+
+    // close the connection
+    close(network_socket);
+
+    // print server response
+    printf("Sever responded with: %s\n ", server_response);
+
+    return 0;
 }
-
-// address struct
-struct sockaddr_in server_address;
-
-server_address.sin_addr.s_addr = inet_addr(SERVER_ADDRESS);
-server_address.sin_port = htons(PORT);
-server_address.sin_family = AF_INET;
-
-
-
-
-
-
-
-int connection_status = connect(network_socket, (struct sockaddr*) &server_address, sizeof(server_address));
-
-if(connection_status == -1){
-
-
-printf("Connection not established, an error occured\n");
-return -1;
-
-}
-printf("Connection established\n");
-char server_response[256];
-char *data;
-data = "HEAD / HTTP/1.0\r\n\r\n";
-
-
-write(network_socket, data, strlen(data));
-memset(server_response, 0, 256);
-read(network_socket, server_response, 255); //exclude null terminator
-
-
-close(network_socket);
-
-printf("Sever responded with: %s\n ", server_response);
-
-return 0;
-
-}
-
-
-
-
-
